22e2da587bd2f0bbac3ebb7b4a119da737aa3959
Niket Shah <shahniket643@gmail.com>
2023-10-17 19:03:47 +0530

    added widgets section, home page complete

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Widgets.css'
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from 'react-twitter-embed';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Widgets = () => {
     return (
-        <div>
-            
+        <div className="widgets">
+            <div className="widgets_input-wrapper">
+                <input type="text" placeholder="Search Twitter" className="widgets_input" />
+                <FontAwesomeIcon icon="search" className="widgets_search"/>
+            </div>
+            <div className="widgets_widget-container">
+                <h2 className="widgets_heading">What's Happening</h2>
+                <TwitterTweetEmbed tweetId='1714118554222580160' options={{ height: 300 }}/>
+                <TwitterTimelineEmbed sourceType='profile' screenName='Niket235' options={{ height: 300 }} />
+                <TwitterShareButton url='https://Niikkk8.github.io/E-Portfolio/#' options={{ text: "This is a tweet directed from Niket's Twitter Clone", height : 50}} style={{padding: '12px 24px'}}/>
+            </div>
         </div>
     );
 }
