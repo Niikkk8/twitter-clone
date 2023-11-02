1a934f4dd5994ceca126bdb00b1e97a217910ae1
Niket Shah <shahniket643@gmail.com>
2023-11-02 21:25:19 +0530

    made changes for hover effects

--- src/components/SidebarOptions.jsx
+++ src/components/SidebarOptions.jsx
@@ -4,7 +4,7 @@ import './SidebarOptions.css';
 
 const SidebarOptions = ({ active, icon, text }) => {
     return (
-        <div className={`sidebar_options ${active && 'sidebar_options-active'}`}>
+        <div className={`sidebar_options ${active ? 'active' : ''}`}>
             <FontAwesomeIcon icon={icon} className="sidebar_icon" />
             <h2 className="sidebar_text">{text}</h2>
         </div>
