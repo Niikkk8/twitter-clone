5d7d2e6352c1a5c2ac03c22726d7ebb38205735b
Niket Shah <shahniket643@gmail.com>
2024-03-30 14:45:43 +0530

    Added sign up functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,21 @@ import { Provider, useSelector } from 'react-redux';
 import { store } from '@/redux/store'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isLoggedIn: boolean = useSelector((state:any) => state.isLoggedIn.isAuthenticated);
+  return (
+    <Provider store={store}>
+      <WrapperComponent Component={Component} pageProps={pageProps} />
+    </Provider>
+  );
+}
+interface WrapperComponentProps {
+  Component: React.ElementType;
+  pageProps: any;
+}
+
+function WrapperComponent({ Component, pageProps }: WrapperComponentProps) {
+  const isLoggedIn: boolean = useSelector((state: any) => state.user.userUID) ? true : false;
+  const username = useSelector((state:any) => state.user.userID)
+  console.log(username)
   const router = useRouter();
 
   useEffect(() => {
@@ -24,12 +38,10 @@ export default function App({ Component, pageProps }: AppProps) {
   const showSidebarAndTrending = isLoggedIn && router.pathname !== '/login';
 
   return (
-    <Provider store={store}>
-      <div className='min-h-screen max-w-[1400px] mx-auto flex'>
-        {showSidebarAndTrending && <Sidebar />}
-        <Component {...pageProps} />
-        {showSidebarAndTrending && <Trending />}
-      </div>
-    </Provider>
-  );
+    <div className='min-h-screen max-w-[1400px] mx-auto flex'>
+      {showSidebarAndTrending && <Sidebar />}
+      <Component {...pageProps} />
+      {showSidebarAndTrending && <Trending />}
+    </div>
+  )
 }
--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  userSlice from './userSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import authenticationSlice from './authenticationSlice';
+import userSlice from './userSlice';
 
 export const store = configureStore({
     reducer: {
-        user: userSlice
-    }
-})
+        user: userSlice,
+        isLoggedIn: authenticationSlice,
+    },
+});
